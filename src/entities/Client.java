b667a8a51693e46b37d197ba2e90810a8bbe8628
alicianunezisaac <alicianunez10492@gmail.com>
2021-02-18 22:34:01 +0100

    client entities

---
+++ src/entities/Client.java
@@ -0,0 +1,55 @@
+package entities;
+
+public class Client {
+
+	public static Client get(long parseLong) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setTipoClient(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setAge(Integer valueOf) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setPassword(int parameter1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setMarried(int parameter1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setFirst_name(int parameter1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object getTipoClient() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setEmail(int parameter1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Long getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setNif(Integer valueOf) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

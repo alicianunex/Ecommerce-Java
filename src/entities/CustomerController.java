7e4a9a3cbcdc21e63623c0f702493ff5e5b0c7f9
Alicia Núñez <69097456+alicianunex@users.noreply.github.com>
2021-02-23 20:50:41 +0100

    Customer Controller

---
+++ src/entities/CustomerController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+
+import com.example.demopagination.entities.Customer;
+
+
+
+
+@Controller
+public class CustomerController {
+
+	
+	@Autowired
+	private CustomerRepository customerRepository;
+
+  @GetMapping("/customers")
+  public String findCustomers(Model model){
+    model.addAttribute("customers", customerRepository.findAll());
+    return "customer-list";
+  }
+}

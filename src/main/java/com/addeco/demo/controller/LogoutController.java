95a3fd809f8044f83971770129da3e9b1d3e87f1
“aymansalem22” <s.aymansalem@gmail.com>
2021-02-26 20:51:28 +0100

    logout second

---
+++ src/main/java/com/addeco/demo/controller/LogoutController.java
@@ -0,0 +1,38 @@
+package com.addeco.demo.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+import com.addeco.demo.repository.CustomerRepository;
+import com.addeco.demo.repository.ProductRepository;
+import com.addeco.demo.repository.ShopCartRepository;
+
+@Controller
+public class LogoutController {
+	
+	@Autowired
+CustomerRepository customerrepo;
+	
+	@Autowired
+ShopCartRepository shopcartrepo;
+	
+	@Autowired
+ProductRepository	productrepo;
+	
+	
+	@GetMapping("/logout")
+	public String logout(Model model, HttpSession session) {
+		 
+		
+		
+		 
+		
+		 return "redirect:/login";
+	}
+
+}

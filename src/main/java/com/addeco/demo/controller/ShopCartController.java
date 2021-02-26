a791b282d3b0e98fea9a7f136e1d0e62a779ea9d
“aymansalem22” <s.aymansalem@gmail.com>
2021-02-26 17:07:13 +0100

    shopcartcontrollerready

--- src/main/java/com/addeco/demo/controller/ShopCartController.java
+++ src/main/java/com/addeco/demo/controller/ShopCartController.java
@@ -11,11 +11,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.addeco.demo.entity.Customer;
 import com.addeco.demo.entity.Product;
 import com.addeco.demo.entity.ShopCart;
 import com.addeco.demo.repository.CustomerRepository;
 import com.addeco.demo.repository.ProductRepository;
 import com.addeco.demo.repository.ShopCartRepository;
+
 
 
 
@@ -67,8 +69,90 @@ ProductRepository	productrepo;
 	
 	
 		
+      @GetMapping("/shopcarts/{id}/deleteproduct")
+      public String deleteToShopCart(@PathVariable long id,Model model,HttpSession session) {
+    	  ShopCart shopcart=(ShopCart)session.getAttribute("shopcart");
+    	  if(shopcart==null)
+    		  return "redirect:/products";
+    	  
+    	  Optional<Product> productopt=productrepo.findById(id);
+    	  if(productopt.isPresent()) {
+    		  Product product=productopt.get();
+         if(shopcart.getProduct().contains(product)) {
+        	 shopcart.getProduct().remove(product);
+        	 
+         }
+    		  
+    	  }
+    	  
+    	  session.setAttribute("shopcart",shopcart);
+    	  model.addAttribute("totalPrice",calculateTotalPrice(shopcart));
+    	  model.addAttribute("shopcart_items",shopcart.getProduct().size());
+    	  model.addAttribute("products",productrepo.findAll());
+    	  return "shopcart";
+    	  
+      }
+      
 		
+      @GetMapping("/shopcart")
+      public String seeShopcart(Model model,HttpSession session) {
+    	  ShopCart shopcart=(ShopCart)session.getAttribute("shopcart");
+    	  if(shopcart==null) {
+    		  
+    		  shopcart=new ShopCart();
+    	  }
+    	  model.addAttribute("totalPrice",calculateTotalPrice(shopcart));
+    	  model.addAttribute("shopcart_items",shopcart.getProduct().size());
+    	  model.addAttribute("shopcart",shopcart);
+    	  return "shopcart";
+    	  
+      }
+      
+      private double calculateTotalPrice(ShopCart shopcart) {
+    	  if(shopcart==null||shopcart.getProduct()==null) {
+    		  return 0;
+    	  }
+    	  double totalPrice=0;
+    	  for(Product product:shopcart.getProduct()) {
+    		  if(product.getPrice()!=null)
+    			  totalPrice+=product.getPrice();
+    		  
+    	  }
+    	  return totalPrice;
+    	  
+      }
+      
+      @GetMapping("/shopcarts/checkout")
+      public String checkout(Model model,HttpSession session) {
+    	  ShopCart shopcart=(ShopCart)session.getAttribute("shopcart");
+    	  Customer customer=(Customer)session.getAttribute("customer");
+    	  if(shopcart==null||customer==null)
+    		  return "redirect:/products";
+    	  
+    	  Optional<Customer> customerDBOpt=customerrepo.findById(customer.getId());
+    	  if(!customerDBOpt.isPresent())
+    		  return "redirect:/products";
+    	  
+    	  shopcart.setCustomers(customerDBOpt.get());
+    	  shopcartrepo.save(shopcart);
+    	  session.removeAttribute("shopcart");
+    	  return "shopcart-checkout";
+    	
+      }
+      
+      @GetMapping("/shopcarts/list")
+      public String showallshopcart(Model model,HttpSession session) {
+    	  Customer customer=(Customer)session.getAttribute("customer");
+    	  model.addAttribute("customer",customer);
+    	  model.addAttribute("usercart",shopcartrepo.findAllByCustomerId(customer.getId()));
+    	  
+    	  return "shopcart-list";
+    	  
+      }
 		
-		
+      
+      
+      
+      
 		
 }
